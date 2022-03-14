e620af9b87ff3967b07e85458206a4ebd0c55d05
Zane Varner <zanevarner0@gmail.com>
2022-03-14 18:40:52 -0400

    Created file main.py for testing of the source file parsing and dataset generation code. Currently, contains a test for parsing a single file.

---
+++ main.py
@@ -0,0 +1,20 @@
+import pprint
+from parse import *
+
+
+def main():
+    # Specify the path to a single source file (for now)
+    source_folder_path = '../data/code2seq/java-small'
+    source_file_path = \
+        f'{source_folder_path}/training/elasticsearch/BulkProcessor.java'
+
+    # List of dictionaries containing the contexts of each method
+    methods = parse_source_file(source_file_path)
+
+    # Print the results in a pretty format
+    pp = pprint.PrettyPrinter(indent=2, sort_dicts=False)
+    pp.pprint(methods)
+
+
+if __name__ == '__main__':
+    main()
